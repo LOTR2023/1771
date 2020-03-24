bbda94aef3695922033bfa61a180ec9d6ee4c013
not_a_seagull <jtnunley01@gmail.com>
2020-03-23 21:24:50 -0700

    Update importshaders.cpp

--- src/render/shaders/importshaders.cpp
+++ src/render/shaders/importshaders.cpp
@@ -1,14 +1,3 @@
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <string>
-
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define VK_APPDATA_VK
-#include "appData.hpp"
-
 #include "importshaders.hpp"
 
 
